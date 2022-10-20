a9123fe03f3724481ba8088cfd587e0c1321c21b
Drew White <d.white0002@gmail.com>
2022-10-20 11:05:07 -0700

    add body to html page and const on js
    Co-authored-by: Anton Ch <anton3ch@icloud.com>
    Co-authored-by: Richelle Thorpe <richelle.ak@gmail.com>
    
    add images to results and bug fixing
    
    Co-authored-by: Anton Ch <anton3ch@icloud.com>
    Co-authored-by: Richelle Thorpe <richelle.ak@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", function () {
+
+  const form = document.querySelector("form");
+  const summary = document.getElementById("summary");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const characterName = document.querySelector("input#characterName").value;
+    const dob = document.querySelector("input#born").value;
+    const radioButton = document.querySelector("input[name='charType']:checked").value;
+    const classType = document.getElementById("classType").value;
+    const hairColor = document.getElementById("color").value;
+    document.querySelector("span#resultName").innerText = characterName;
+    document.querySelector("span#resultDob").innerText = dob;
+    document.getElementById("resultCharType").innerText = radioButton;
+    document.querySelector("span#resultClassType").innerText = classType;
+    document.querySelector("span#resultColor").innerText = hairColor;
+    summary.removeAttribute("class");
+
+    if (radioButton === "orc") {
+      document.getElementById("orcImage").removeAttribute("class");
+    } else if (radioButton === "elf") {
+      document.getElementById("elfImage").removeAttribute("class");
+    } else if (radioButton === "dwarf") {
+      document.getElementById("dwarfImage").removeAttribute("class");
+    } else if (radioButton === "mortal") {
+      document.getElementById("mortalImage").removeAttribute("class");
+    }
+
+
+  }
+  )
+
+})
